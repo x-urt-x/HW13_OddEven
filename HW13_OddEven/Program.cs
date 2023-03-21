a32ef37179ccf3868768965f211d2e062421bccf
urt <urt5002urt@gmail.com>
2023-03-21 17:28:56 +0200

    add all

---
+++ HW13_OddEven/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace HW13_OddEven
+{
+    internal class Program
+    {
+        static Mutex mutex = new Mutex();
+        static void Main(string[] args)
+        {
+            var taskOdd = new Task(() =>
+        {
+            for (int i = 1; i <= 99; i += 2)
+            {
+                Print(i);
+            }
+        });
+            var taskEven = new Task(() =>
+            {
+                for (int i = 2; i <= 100; i += 2)
+                {
+                    Print(i);
+                }
+            });
+            taskOdd.Start();
+            taskEven.Start();
+            Console.ReadLine();
+        }
+        static void Print(int i)
+        {
+            mutex.WaitOne();
+            Console.WriteLine(i);
+            mutex.ReleaseMutex();
+        }
+    }
+}
